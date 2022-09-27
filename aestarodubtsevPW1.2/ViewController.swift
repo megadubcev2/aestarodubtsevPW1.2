966fd2404412fed87163b56797bf2ad6ccf93ab2
megadubcev2 <aestarodubtsev@edu.hse.ru>
2022-09-27 23:30:14 +0300

    Initial Commit

---
+++ aestarodubtsevPW1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aestarodubtsevPW1.2
+//
+//  Created by Andrew on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
